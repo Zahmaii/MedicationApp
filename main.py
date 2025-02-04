3b5a737bd00ae6179d9bd6ac7b5741f7565637ac
Zahmaii <168623030+Zahmaii@users.noreply.github.com>
2025-02-04 15:21:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import streamlit as st
 from streamlit_option_menu import option_menu
 from matplotlib.colors import LinearSegmentedColormap
